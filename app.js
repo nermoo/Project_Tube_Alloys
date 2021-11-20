3dfdf3da6f705a7c1f1e0a8d0561cef169102bb6
nermoo <53093663+nermoo@users.noreply.github.com>
2021-11-20 23:36:45 +0530

    update the port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const cors=require('cors');
 const cookieParser=require('cookie-parser');
 const path = require('path');
 
-const PORT=5000;;
+const PORT=process.env.PORT || 5000;;
 app.use(function(req, res, next) {
   // res.header("Access-Control-Allow-Origin", "http://localhost:5000"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
